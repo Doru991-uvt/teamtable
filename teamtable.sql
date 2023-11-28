149bbfd5f3664e56ec990ace775e2d461976cc8b
Doru991-uvt <147608155+Doru991-uvt@users.noreply.github.com>
2023-11-28 05:50:22 +0200

    Add files via upload

---
+++ teamtable.sql
@@ -0,0 +1,174 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 28, 2023 at 04:46 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `teamtable`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `courses`
+--
+
+CREATE TABLE `courses` (
+  `cid` int(11) NOT NULL,
+  `pid` int(11) NOT NULL,
+  `cname` varchar(50) NOT NULL,
+  `inter_orar` int(11) NOT NULL,
+  `grup` int(11) NOT NULL,
+  `curs` tinyint(1) NOT NULL DEFAULT 0,
+  `spsi` set('si','sp','na') NOT NULL DEFAULT 'na'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `grupe`
+--
+
+CREATE TABLE `grupe` (
+  `relid` int(11) NOT NULL,
+  `sid` int(11) NOT NULL,
+  `grupa` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `modificari`
+--
+
+CREATE TABLE `modificari` (
+  `mid` int(11) NOT NULL,
+  `uid` int(11) NOT NULL,
+  `cid` int(11) NOT NULL,
+  `toint` int(11) DEFAULT NULL,
+  `start` date NOT NULL DEFAULT current_timestamp(),
+  `fin` date NOT NULL DEFAULT current_timestamp(),
+  `global` tinyint(1) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sessions`
+--
+
+CREATE TABLE `sessions` (
+  `sid` int(11) NOT NULL,
+  `uid` int(11) NOT NULL,
+  `ip` varchar(15) NOT NULL,
+  `token` varchar(50) NOT NULL,
+  `createdate` date NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `uid` int(11) NOT NULL,
+  `email` varchar(40) NOT NULL,
+  `nume` varchar(40) NOT NULL,
+  `tip_cont` set('prof','stud','admin') NOT NULL DEFAULT 'stud',
+  `passwd` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`uid`, `email`, `nume`, `tip_cont`, `passwd`) VALUES
+(1, 'root@teamtable.ro', 'root', 'admin', 'root');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `courses`
+--
+ALTER TABLE `courses`
+  ADD PRIMARY KEY (`cid`);
+
+--
+-- Indexes for table `grupe`
+--
+ALTER TABLE `grupe`
+  ADD PRIMARY KEY (`relid`);
+
+--
+-- Indexes for table `modificari`
+--
+ALTER TABLE `modificari`
+  ADD PRIMARY KEY (`mid`);
+
+--
+-- Indexes for table `sessions`
+--
+ALTER TABLE `sessions`
+  ADD PRIMARY KEY (`sid`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`uid`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `courses`
+--
+ALTER TABLE `courses`
+  MODIFY `cid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+
+--
+-- AUTO_INCREMENT for table `grupe`
+--
+ALTER TABLE `grupe`
+  MODIFY `relid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+
+--
+-- AUTO_INCREMENT for table `modificari`
+--
+ALTER TABLE `modificari`
+  MODIFY `mid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+
+--
+-- AUTO_INCREMENT for table `sessions`
+--
+ALTER TABLE `sessions`
+  MODIFY `sid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `uid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
